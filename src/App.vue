909d9127cec82eab89ffcff38d5f4c800aeb9ff1
Kate <katshe18@gmail.com>
2024-11-22 11:17:11 +1300

    Create Vue project, componets, styles

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'App',
+    components: {
+        Header,
+    },
+};
+import Header from './components/Header/Header.vue';
+</script>
+
+<template>
+    <div id="app">
+        <h1>Hello world here is the App</h1>
+        <Header />
+    </div>
+</template>
