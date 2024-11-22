d342f11cc46de7a3aef8db1abdd5e48e98c40567
Kate <katshe18@gmail.com>
2024-11-22 15:55:01 +1300

    Add API call to fetch product data

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@ export default {
         Product,
     },
 };
+
+async function fetchProductData() {
+    try {
+        const res = await fetch('https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com/live/product');
+        const productData = await res.json();
+        console.log(productData);
+        return productData;
+    } catch (error) {
+        console.log('error fetching product data', error);
+        return null;
+    }
+}
+fetchProductData();
+
 </script>
 
 <template>
